9c931d296920b16f1f2d6c6495e860557d67703a
Myxbaah <admin@myxbaah.xyz>
2022-04-29 17:50:27 +0500

    Parse csv file into nodes and links

--- ForceTree/ForceTree.js
+++ ForceTree/ForceTree.js
@@ -10,13 +10,22 @@ export async function forceTree(params) {
 
     try {
         const data = await d3.csv("http://localhost:3000/static/datakegiatanorganisasimhs_2016-2020.csv");
-        console.log(data);
-        const root = d3.hierarchy(data);
-        const links = root.links();
-        const nodes = root.descendants(); 
+
+        let nodes = [];
+        let links = [];
+        data.forEach(d => {
+            if(nodes.indexOf(d.mhs_nrp.trim()) < 0) {
+              nodes.push(d.mhs_nrp.trim());
+            }
+            if(nodes.indexOf(d.keg_kode.trim()) < 0) {
+              nodes.push(d.keg_kode.trim());
+            }
+            links.push({source: nodes.indexOf(d.mhs_nrp.trim()), target: nodes.indexOf(d.keg_kode.trim())});
+        });
+        nodes = nodes.map(n => {return {name: n}});
 
         const simulation = d3.forceSimulation(nodes)
-            .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(1))
+            .force("link", d3.forceLink(links).id(d => d.index).distance(0).strength(1))
             .force("charge", d3.forceManyBody().strength(-50))
             .force("x", d3.forceX())
             .force("y", d3.forceY());
@@ -38,25 +47,25 @@ export async function forceTree(params) {
         .selectAll("circle")
         .data(nodes)
         .join("circle")
-            .attr("fill", d => d.children ? null : "#000")
-            .attr("stroke", d => d.children ? null : "#fff")
-            .attr("r", 3.5);
+        .attr("fill", "#000")
+        .attr("stroke", "#fff")
+        .attr("r", 3.5);
     
         node.append("title")
-            .text(d => d.data.name);
+            .text(d => d.name);
     
         simulation.on("tick", () => {
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
-    
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-        });
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
         
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
+
         return body.html();
     } catch(e) {
         console.error("Error: " + e);

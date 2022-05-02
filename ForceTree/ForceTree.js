4f0e8bfe21946c18ad264a9bb75f3797e9e9c7a5
Ryuubii <prexordrake@gmail.com>
2022-05-02 15:36:45 +0700

    tried fix, not working.

--- ForceTree/ForceTree.js
+++ ForceTree/ForceTree.js
@@ -49,7 +49,8 @@ export async function forceTree(params) {
         .join("circle")
         .attr("fill", "#000")
         .attr("stroke", "#fff")
-        .attr("r", 3.5);
+        .attr("r", 3.5)
+        .call(drag(simulation));
     
         node.append("title")
             .text(d => d.name);
@@ -72,3 +73,28 @@ export async function forceTree(params) {
         return {};
     }
 }
+
+function drag(simulation){
+  
+    function dragstarted(event, d) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+    
+    function dragged(event, d) {
+      d.fx = event.x;
+      d.fy = event.y;
+    }
+    
+    function dragended(event, d) {
+      if (!event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+    
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+  }

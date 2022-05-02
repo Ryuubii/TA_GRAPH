c0cb4131f14eeec55c0d197b276b35ec78bfa9f6
Ryuubii <prexordrake@gmail.com>
2022-05-02 15:44:22 +0700

    Fixed, maybe?

--- ForceTree/ForceTree.js
+++ ForceTree/ForceTree.js
@@ -49,13 +49,12 @@ export async function forceTree(params) {
         .join("circle")
         .attr("fill", "#000")
         .attr("stroke", "#fff")
-        .attr("r", 3.5)
-        .call(drag(simulation));
+        .attr("r", 3.5);
     
         node.append("title")
             .text(d => d.name);
     
-        simulation.on("tick", () => {
+        node.call(() => {
             link
                 .attr("x1", d => d.source.x)
                 .attr("y1", d => d.source.y)
@@ -73,28 +72,3 @@ export async function forceTree(params) {
         return {};
     }
 }
-
-function drag(simulation){
-  
-    function dragstarted(event, d) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-    
-    function dragged(event, d) {
-      d.fx = event.x;
-      d.fy = event.y;
-    }
-    
-    function dragended(event, d) {
-      if (!event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
-    
-    return d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended);
-  }

d4eb6971fe41c3d908c29db192361a3407e7f7a9
Ryuubii <prexordrake@gmail.com>
2022-05-07 17:46:38 +0700

    Cyto3

--- Cytoscape/Cytoscape.js
+++ Cytoscape/Cytoscape.js
@@ -7,5 +7,15 @@ export async function cyto(params) {
   var cy = cytoscape({});
 
   const data = await readCsv("datakegiatanorganisasimhs_2016-2020.csv");
-  console.log(data);
+  for (let i = 0; i < data.length; i++) {
+      cy.add([
+          {group: 'nodes', data: {id: data[i].mhs_nrp}},
+          {group: 'nodes', data: {id: data[i].keg_kode}},
+          {group: 'edges', data: {id: 'e'+i, source: data[i].mhs_nrp, target: data[i].keg_kode}}
+      ])
+  }
+
+  var clusters = cy.elements().markovCluster();
+
+  return clusters;
 }

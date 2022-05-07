9215f91901f43bc7d3f51645a16f9e7828ba48ed
Myxbaah <admin@myxbaah.xyz>
2022-05-07 15:21:33 +0500

    CSV reading from public folder

--- Cytoscape/Cytoscape.js
+++ Cytoscape/Cytoscape.js
@@ -1,15 +1,12 @@
 import cytoscape from 'cytoscape';
 import markovCluster from 'cytoscape-markov-cluster';
 import fs from 'fs';
+import { readCsv } from '../Helpers/ReadCsv.js';
 markovCluster(cytoscape);
 
 export async function cyto(params) {
     var cy = cytoscape({});
 
-    fs.readFile('/public/datakegiatanorganisasimhs_2016-2020.csv', 'utf8', function (err, data) {
-        var dataArray = data.split(/\r?\n/);  //Be careful if you are in a \r\n world...
-        // Your array contains ['ID', 'D11', ... ]
-        console.log(data);
-    });
-    
+    const data = await readCsv("datakegiatanorganisasimhs_2016-2020.csv");
+    console.log(data);
 }

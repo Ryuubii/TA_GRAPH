dc1f3db12f5662019b8a466e2c91625d78855476
Myxbaah <admin@myxbaah.xyz>
2022-05-07 15:31:13 +0500

    Prettier formatting

--- Cytoscape/Cytoscape.js
+++ Cytoscape/Cytoscape.js
@@ -1,11 +1,11 @@
-import cytoscape from 'cytoscape';
+import cytoscape from "cytoscape";
 import markovCluster from "cytoscape-markov-cluster";
 import { readCsv } from "../Helpers/ReadCsv.js";
 markovCluster(cytoscape);
 
 export async function cyto(params) {
-    var cy = cytoscape({});
+  var cy = cytoscape({});
 
-    const data = await readCsv("datakegiatanorganisasimhs_2016-2020.csv");
-    console.log(data);
+  const data = await readCsv("datakegiatanorganisasimhs_2016-2020.csv");
+  console.log(data);
 }

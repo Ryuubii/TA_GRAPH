f34cd18f1220edb1a3f573ca0faa461094e1f3ad
Ryuubii <prexordrake@gmail.com>
2022-05-08 21:20:28 +0700

    Closeness Centrality done

--- Cytoscape/Cytoscape.js
+++ Cytoscape/Cytoscape.js
@@ -13,34 +13,39 @@ export async function cyto(params) {
     let elesLink = [];
     const data = await readCsv("datakegiatanorganisasimhs_2016-2020.csv");
     const eles = getEles(data);
-    console.log(eles);
+    cy.add(eles);
+    // console.log(eles);
     // nodes.forEach((node) => {
-    //     // cy.add({group: 'nodes', data: {id: node.name}});
-    //     elesNode.push(node.name);
+    //     cy.add({group: 'nodes', data: {id: node.name}});
+    //     // elesNode.push(node.name);
     //     console
     // })
     // var ctr = 0;
-    // links.forEach((link) => {
-    //     // cy.add({group: 'edges', data: {id: 'e'+ctr, source: link.source, target: link.target}});
-    //     elesLink.push({
-    //         id: 'e'+ctr,
-    //         source: link.source,
-    //         target: link.target
-    //     })
-    //     ctr++;
+    // edges.forEach((link) => {
+    //     cy.add({group: 'edges', data: {id: link.id, source: link.source, target: link.target}});
+    //     // elesLink.push({
+    //     //     id: 'e'+ctr,
+    //     //     source: link.source,
+    //     //     target: link.target
+    //     // })
+    //     // ctr++;
     // })
 
     let ccn = cy.elements().closenessCentralityNormalized({ /* my options */ });
-
+    let closeness = []
     cy.nodes().forEach( n => {
     n.data({
         ccn: ccn.closeness( n )
     });
+    closeness.push({
+        id: n.data().id,
+        ccn: ccn.closeness( n )
+    })
     } );
 
-    // return cy.data();
-    console.log(elesNode);
-    console.log("LINKS")
-    console.log(elesLink);
-    return elesNode;
+    return closeness;
+    // console.log(elesNode);
+    // console.log("LINKS")
+    // console.log(elesLink);
+    // return elesNode;
 }

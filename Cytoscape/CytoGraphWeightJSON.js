953109fa17aab54fe5778fb22cbaec4ade8cf121
Myxbaah <admin@myxbaah.xyz>
2022-05-13 21:03:34 +0500

    Fix json processing. Check for number type

--- Cytoscape/CytoGraphWeightJSON.js
+++ Cytoscape/CytoGraphWeightJSON.js
@@ -1,18 +1,8 @@
 import cytoscape from "cytoscape";
-import { readCsv } from "../Helpers/ReadCsv.js";
 import { getEles } from "../Helpers/GetEles.js";
 import { stringify } from 'flatted';
 
-const body = window.document.getElementsByTagName("body")[0];
-body.insertAdjacentHTML("afterbegin", `<div id="cy" style="width: 100%; height: 100%; display: block;"></div>`);
-Object.defineProperty(window.HTMLHtmlElement.prototype, 'clientWidth', {value: 1920});
-Object.defineProperty(window.HTMLHtmlElement.prototype, 'clientHeight', {value: 1080});
-Object.defineProperty(document.getElementById('cy'), 'offsetWidth', {value: 1920});
-Object.defineProperty(document.getElementById('cy'), 'offsetHeight', {value: 1080});
-
 export async function cytoGraphWeightJSON(params) {
-    console.log(document.getElementById('cy').offsetWidth);
-    console.log(document.getElementById('cy').offsetHeight);
 
     let cy = cytoscape({
         // container: document.getElementById("cy"),
@@ -28,9 +18,8 @@ export async function cytoGraphWeightJSON(params) {
     })
 
     const markov = cy.elements().markovClustering();
-    // console.log(markov);
     const jsonified = stringify(markov)
-    const l = await cy.layout({
+    const l = cy.layout({
         name: "random"
     }).run();
 
